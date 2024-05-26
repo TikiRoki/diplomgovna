371646fd5d837b7f1c230c15b0c75096612db2b9
Марат Негиматжанов <torbenborstl@outlook.com>
2024-05-27 00:41:46 +0300

    Фото

--- app/page.js
+++ app/page.js
@@ -48,135 +48,133 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <Carousel images={images} />
-      <div className="bg-gray-200 p-8 rounded-lg shadow-lg w-96 z-10">
-        <h1 className="text-2xl font-bold mb-6">Оценка стоимости дома</h1>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="location"
-          >
-            Расположение
-          </label>
-          <input
-            type="text"
-            id="location"
-            name="location"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Введите расположение"
-            value={data.location}
-            onChange={handleChange}
-          />
-          {/* <button onClick={getCoordinates}>Получить координаты</button>
-          {coordinates && (
-            <div>
-              <p>Широта: {coordinates.latitude}</p>
-              <p>Долгота: {coordinates.longitude}</p>
+      <div className=" z-50 flex flex-row justify-evenly w-[1000px] relative">
+        <div className="w-full">
+          <img className="w-full z-50" src="./1.jpg" />
+        </div>
+        <div className="bg-gray-200 p-8 rounded-lg shadow-lg w-96 z-10">
+          <h1 className="text-2xl font-bold mb-6">Оценка стоимости дома</h1>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="location"
+            >
+              Расположение
+            </label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Введите расположение"
+              value={data.location}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4 relative">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="type"
+            >
+              Тип дома
+            </label>
+            <select
+              id="type"
+              name="type"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={data.type}
+              onChange={handleChange}
+            >
+              <option value={0}>Панельный</option>
+              <option value={1}>Монолитный</option>
+              <option value={2}>Кирпичный</option>
+              <option value={3}>Блочный</option>
+              <option value={4}>Деревянный</option>
+            </select>
+            <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
+              </svg>
             </div>
-          )} */}
-        </div>
-        <div className="mb-4 relative">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="type"
-          >
-            Тип дома
-          </label>
-          <select
-            id="type"
-            name="type"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={data.type}
-            onChange={handleChange}
-          >
-            <option value={0}>Панельный</option>
-            <option value={1}>Монолитный</option>
-            <option value={2}>Кирпичный</option>
-            <option value={3}>Блочный</option>
-            <option value={4}>Деревянный</option>
-          </select>
-          <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
-            </svg>
           </div>
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="floor"
-          >
-            Этаж
-          </label>
-          <input
-            type="number"
-            id="floor"
-            name="floor"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Введите этаж"
-            value={data.floor}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4 relative">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="rooms"
-          >
-            Количество комнат
-          </label>
-          <select
-            id="rooms"
-            name="rooms"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={data.rooms}
-            onChange={handleChange}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4+">4+</option>
-          </select>
-          <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="floor"
             >
-              <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
-            </svg>
+              Этаж
+            </label>
+            <input
+              type="number"
+              id="floor"
+              name="floor"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Введите этаж"
+              value={data.floor}
+              onChange={handleChange}
+            />
           </div>
+          <div className="mb-4 relative">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="rooms"
+            >
+              Количество комнат
+            </label>
+            <select
+              id="rooms"
+              name="rooms"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={data.rooms}
+              onChange={handleChange}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4+">4+</option>
+            </select>
+            <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
+              </svg>
+            </div>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="area"
+            >
+              Площадь
+            </label>
+            <input
+              type="number"
+              id="area"
+              name="area"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Введите площадь"
+              value={data.area}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={handleSubmit}
+            >
+              Оценить стоимость
+            </button>
+          </div>
+          {cost && <div className=" ">{cost}</div>}
         </div>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="area"
-          >
-            Площадь
-          </label>
-          <input
-            type="number"
-            id="area"
-            name="area"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Введите площадь"
-            value={data.area}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            onClick={handleSubmit}
-          >
-            Оценить стоимость
-          </button>
-        </div>
-        {cost && <div className=" ">{cost}</div>}
       </div>
     </div>
   );
--- pages/api/getCost.js
+++ pages/api/getCost.js
@@ -2,7 +2,9 @@ import tf, { tensor } from "@tensorflow/tfjs";
 import fs from "fs";
 import csv from "csv-parser";
 
-async function loadPartialData(filePath, limit = 10000) {
+const types = ["Квартира", "Многоквартирный дом"];
+
+async function loadPartialData(filePath, limit = 1000) {
   return new Promise((resolve, reject) => {
     const data = [];
     let count = 0;
@@ -13,14 +15,19 @@ async function loadPartialData(filePath, limit = 10000) {
       .pipe(csvParser)
       .on("data", (row) => {
         if (count < limit) {
+          const type = types.indexOf(row.type) >= 0 ? row.type : false;
+          const rooms = parseFloat(row.rooms) >= 1 ? row.type : false;
+          const [level, maxLevel] = row.level.split("/").map(Number)
+          
+
           data.push({
-            level: parseFloat(row.level),
-            rooms: parseInt(row.rooms) === -1 ? 0 : parseInt(row.rooms), // Студии преобразуем в 0
-            area: parseFloat(row.area),
-            object_type: parseFloat(row.object_type),
-            geo_lat: parseFloat(row.geo_lat),
-            geo_lon: parseFloat(row.geo_lon),
-            price: parseFloat(row.price),
+            // level: parseFloat(row.level),
+            // rooms: parseInt(row.rooms) === -1 ? 0 : parseInt(row.rooms), // Студии преобразуем в 0
+            // area: parseFloat(row.area),
+            // object_type: parseFloat(row.object_type),
+            // geo_lat: parseFloat(row.geo_lat),
+            // geo_lon: parseFloat(row.geo_lon),
+            // price: parseFloat(row.price),
           });
           count++;
         }
@@ -36,116 +43,106 @@ async function loadPartialData(filePath, limit = 10000) {
   });
 }
 
-const normalize = (tensor) => {
-  const res = tf.tidy(() => {
-    // Создание substract tensor и range size
-    const SUBSTRACT_TENSOR = tf.sub(tensor, MIN_INPUT_VALUE);
-    const RANGE_SIZE = tf.sub(MAX_INPUT_VALUE, MIN_INPUT_VALUE);
-    const NORMALIZED_TENSOR = tf.div(SUBSTRACT_TENSOR, RANGE_SIZE);
-    return NORMALIZED_TENSOR;
-  });
-
-  return res;
-};
-
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    if (
-      req.query.floor &&
-      req.query.rooms &&
-      req.query.area &&
-      req.query.type &&
-      req.query.lat &&
-      req.query.lon
-    ) {
-      const data = await loadPartialData("./static/all_v2.csv");
+    // if (
+    //   req.query.floor &&
+    //   req.query.rooms &&
+    //   req.query.area &&
+    //   req.query.type &&
+    //   req.query.lat &&
+    //   req.query.lon
+    // ) {
+    //   const data = await loadPartialData("./static/");
 
-      const features = data.map((d) => [
-        d.level,
-        d.rooms,
-        d.area,
-        d.object_type,
-        d.geo_lat,
-        d.geo_lon,
-      ]);
-      const labels = data.map((d) => d.price);
+    //   const features = data.map((d) => [
+    //     d.level,
+    //     d.rooms,
+    //     d.area,
+    //     d.object_type,
+    //     d.geo_lat,
+    //     d.geo_lon,
+    //   ]);
+    //   const labels = data.map((d) => d.price);
 
-      const INPUT_TENSOR = tf.tensor2d(features);
-      const OUTPUT_TENSOR = tf.tensor1d(labels);
+    //   const INPUT_TENSOR = tf.tensor2d(features);
+    //   const OUTPUT_TENSOR = tf.tensor1d(labels);
 
-      const MIN_INPUT_VALUE = tf.min(INPUT_TENSOR, 0);
-      const MAX_INPUT_VALUE = tf.max(INPUT_TENSOR, 0);
+    //   const MIN_INPUT_VALUE = tf.min(INPUT_TENSOR, 0);
+    //   const MAX_INPUT_VALUE = tf.max(INPUT_TENSOR, 0);
 
-      const normalize = (tensor) => {
-        const res = tf.tidy(() => {
-          // Создание substract tensor и range size
-          const SUBSTRACT_TENSOR = tf.sub(tensor, MIN_INPUT_VALUE);
-          const RANGE_SIZE = tf.sub(MAX_INPUT_VALUE, MIN_INPUT_VALUE);
-          const NORMALIZED_TENSOR = tf.div(SUBSTRACT_TENSOR, RANGE_SIZE);
-          return NORMALIZED_TENSOR;
-        });
+    //   const normalize = (tensor) => {
+    //     const res = tf.tidy(() => {
+    //       // Создание substract tensor и range size
+    //       const SUBSTRACT_TENSOR = tf.sub(tensor, MIN_INPUT_VALUE);
+    //       const RANGE_SIZE = tf.sub(MAX_INPUT_VALUE, MIN_INPUT_VALUE);
+    //       const NORMALIZED_TENSOR = tf.div(SUBSTRACT_TENSOR, RANGE_SIZE);
+    //       return NORMALIZED_TENSOR;
+    //     });
 
-        return res;
-      };
-      const NORMALIZER_INPUT_TENSOR = normalize(INPUT_TENSOR);
+    //     return res;
+    //   };
+    //   const NORMALIZER_INPUT_TENSOR = normalize(INPUT_TENSOR);
 
-      let model = tf.sequential();
-      model.add(tf.layers.dense({ inputShape: [6], units: 1 }));
+    //   let model = tf.sequential();
+    //   model.add(tf.layers.dense({ inputShape: [6], units: 1 }));
 
-      const train = async () => {
-        // Компиляция модели
-        // Указание оптимизватора и функции потерь
-        model.compile({
-          optimizer: tf.train.sgd(0.001), // learning rate = 0.01,
-          loss: "meanSquaredError",
-        });
-        //Обучение
-        await model.fit(NORMALIZER_INPUT_TENSOR, OUTPUT_TENSOR, {
-          batchSize: 128,
-          epochs: 30,
-          shuffle: true,
-        });
+    //   const train = async () => {
+    //     // Компиляция модели
+    //     // Указание оптимизватора и функции потерь
+    //     model.compile({
+    //       optimizer: tf.train.sgd(0.001), // learning rate = 0.01,
+    //       loss: "meanSquaredError",
+    //     });
+    //     //Обучение
+    //     await model.fit(NORMALIZER_INPUT_TENSOR, OUTPUT_TENSOR, {
+    //       batchSize: 128,
+    //       epochs: 30,
+    //       shuffle: true,
+    //     });
 
-        // Удаление промежуточных тензоров
-        NORMALIZER_INPUT_TENSOR.dispose();
-        OUTPUT_TENSOR.dispose();
-      };
+    //     // Удаление промежуточных тензоров
+    //     NORMALIZER_INPUT_TENSOR.dispose();
+    //     OUTPUT_TENSOR.dispose();
+    //   };
 
-      await train();
+    //   await train();
 
-      const tryToPredict = (array) => {
-        return tf.tidy(() => {
-          let input = normalize(tf.tensor2d(array));
+    //   const tryToPredict = (array) => {
+    //     return tf.tidy(() => {
+    //       let input = normalize(tf.tensor2d(array));
 
-          let output = model.predict(input);
+    //       let output = model.predict(input);
 
-          output.print();
-          const outputData = output.dataSync(); // Получаем массив значений
+    //       output.print();
+    //       const outputData = output.dataSync(); // Получаем массив значений
 
-          // Предположим, что модель возвращает один результат
-          const result = outputData[0];
+    //       // Предположим, что модель возвращает один результат
+    //       const result = outputData[0];
 
-          return result;
-        });
-      };
+    //       return result;
+    //     });
+    //   };
 
-      // Этаж, комнаты, площадь, тип постройки, lat, lon
+    //   // Этаж, комнаты, площадь, тип постройки, lat, lon
 
-      const cost = tryToPredict([
-        [
-          Number(req.query.floor),
-          Number(req.query.rooms),
-          Number(req.query.area),
-          Number(req.query.type),
-          Number(req.query.lat),
-          Number(req.query.lon),
-        ],
-      ]);
+    //   const cost = tryToPredict([
+    //     [
+    //       Number(req.query.floor),
+    //       Number(req.query.rooms),
+    //       Number(req.query.area),
+    //       Number(req.query.type),
+    //       Number(req.query.lat),
+    //       Number(req.query.lon),
+    //     ],
+    //   ]);
 
-      return res.status(200).json({ cost: cost });
-    } else {
-      return res.status(400).json({ error: "Bad request" });
-    }
+    //   return res.status(200).json({ cost: cost });
+    // } else {
+    //   return res.status(400).json({ error: "Bad request" });
+    // }
+
+    return res.status(200).json({ cost: "" });
   }
   return res.status(401).json({ message: "Method not allowed" });
 }
