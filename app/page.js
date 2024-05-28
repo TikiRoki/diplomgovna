236333ce2b75020dd5bc61dd70ea25f3f841bbfa
TikiToki <crisdev3214@gmail.com>
2024-05-28 23:01:26 +0300

    diplom28may

--- app/page.js
+++ app/page.js
@@ -15,6 +15,7 @@ export default function Home() {
     area: 0,
   });
   const [cost, setCost] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
@@ -43,141 +44,203 @@ export default function Home() {
       .then((res) => res.data);
 
     setCost(result.cost);
+    setIsModalOpen(true); // Открыть модальное окно после получения результата
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100 py-16">
-      <Carousel images={images} />
-      <div className=" z-50 flex flex-row w-[1000px] relative h-full my-16 border-8 border-solid border-[] rounded-2xl">
-        <div className="w-full h-full">
-          <img
-            className="object-cover w-full h-full z-50 rounded-l-lg"
-            src="./FON.jpg"
-          />
-        </div>
-        <div className="bg-gray-200 p-8 rounded-r-lg shadow-lg w-full z-10">
-          <h1 className="text-2xl font-bold mb-6">Оценка стоимости дома</h1>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="location"
-            >
-              Расположение
-            </label>
-            <input
-              type="text"
-              id="location"
-              name="location"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Введите расположение"
-              value={data.location}
-              onChange={handleChange}
-            />
+    <div className="relative flex flex-col min-h-screen">
+      {/* Fixed Carousel Background */}
+      <div className="fixed inset-0 z-0">
+        <Carousel images={images} />
+      </div>
+
+      {/* Overlay for content */}
+      <div className="relative z-10 flex flex-col min-h-screen bg-opacity-60 backdrop-blur-">
+        {/* Header */}
+        <header className="bg-gray-800 text-white py-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <div className="text-2xl font-bold">
+              <img src="/LOGO.png" alt="Logo" className="h-10 w-100 inline-block" />
+            </div>
+            <nav>
+              <ul className="flex space-x-4">
+                <li>
+                  <a href="#" className="hover:underline">Главная</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">О нас</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">Контакты</a>
+                </li>
+              </ul>
+            </nav>
           </div>
-          <div className="mb-4 relative">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="type"
-            >
-              Тип дома
-            </label>
-            <select
-              id="type"
-              name="type"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={data.type}
-              onChange={handleChange}
-            >
-              <option value={0}>Панельный</option>
-              <option value={1}>Монолитный</option>
-              <option value={2}>Кирпичный</option>
-              <option value={3}>Блочный</option>
-              <option value={4}>Деревянный</option>
-            </select>
-            <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
-              </svg>
+        </header>
+
+        {/* Main Content */}
+        <main className="flex flex-1 justify-center items-center">
+          <div className="relative w-full h-full">
+            <div className="flex flex-col items-center justify-center w-full h-full">
+              <div className="z-50 flex flex-col md:flex-row w-full max-w-4xl h-auto bg-white shadow-lg rounded-lg overflow-hidden">
+                <div className="hidden md:block md:w-1/2">
+                  <img
+                    className="object-cover w-full h-full"
+                    src="./FON.jpg"
+                    alt="FON"
+                  />
+                </div>
+                <div className="w-full md:w-1/2 p-8">
+                  <h1 className="text-2xl font-bold mb-6">Оценка стоимости жилья</h1>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="location"
+                    >
+                      Расположение
+                    </label>
+                    <input
+                      type="text"
+                      id="location"
+                      name="location"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      placeholder="Введите расположение"
+                      value={data.location}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="mb-4 relative">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="type"
+                    >
+                      Тип дома
+                    </label>
+                    <select
+                      id="type"
+                      name="type"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      value={data.type}
+                      onChange={handleChange}
+                    >
+                      <option value={0}>Панельный</option>
+                      <option value={1}>Монолитный</option>
+                      <option value={2}>Кирпичный</option>
+                      <option value={3}>Блочный</option>
+                      <option value={4}>Деревянный</option>
+                    </select>
+                    <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
+                      <svg
+                        className="fill-current h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="floor"
+                    >
+                      Этаж
+                    </label>
+                    <input
+                      type="number"
+                      id="floor"
+                      name="floor"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      placeholder="Введите этаж"
+                      value={data.floor}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="mb-4 relative">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="rooms"
+                    >
+                      Количество комнат
+                    </label>
+                    <select
+                      id="rooms"
+                      name="rooms"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      value={data.rooms}
+                      onChange={handleChange}
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4+">4+</option>
+                    </select>
+                    <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
+                      <svg
+                        className="fill-current h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="area"
+                    >
+                      Площадь
+                    </label>
+                    <input
+                      type="number"
+                      id="area"
+                      name="area"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      placeholder="Введите площадь"
+                      value={data.area}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="flex justify-center">
+                    <button
+                      type="submit"
+                      className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      onClick={handleSubmit}
+                    >
+                      Оценить стоимость
+                    </button>
+                  </div>
+                  {isModalOpen && (
+                    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+                      <div className="bg-white p-8 rounded-lg shadow-lg w-80">
+                        <h2 className="text-xl font-bold mb-4">Примерная стоимость жилья</h2>
+                        <p className="mb-6">{cost} рублей</p>
+                        <button
+                          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                          onClick={closeModal}
+                        >
+                          Закрыть
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="floor"
-            >
-              Этаж
-            </label>
-            <input
-              type="number"
-              id="floor"
-              name="floor"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Введите этаж"
-              value={data.floor}
-              onChange={handleChange}
-            />
+        </main>
+
+        {/* Footer */}
+        <footer className="bg-gray-800 text-white py-4">
+          <div className="container mx-auto text-center">
+            <p>&copy; 2024 Ваш сайт. Все права защищены.</p>
           </div>
-          <div className="mb-4 relative">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="rooms"
-            >
-              Количество комнат
-            </label>
-            <select
-              id="rooms"
-              name="rooms"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={data.rooms}
-              onChange={handleChange}
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4+">4+</option>
-            </select>
-            <div className="pointer-events-none absolute top-1/2 transform -translate-y-2/2 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M5.59 7.41L10 11.83l4.41-4.42L16 8.83l-6 6-6-6z" />
-              </svg>
-            </div>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="area"
-            >
-              Площадь
-            </label>
-            <input
-              type="number"
-              id="area"
-              name="area"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Введите площадь"
-              value={data.area}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              onClick={handleSubmit}
-            >
-              Оценить стоимость
-            </button>
-          </div>
-          {cost && <div className=" ">{cost}</div>}
-        </div>
+        </footer>
       </div>
     </div>
   );
